2118833f4e05104605374f185b8f23e47db80bf1
Wilman Sirit <wsirit@gmail.com>
2021-09-29 20:19:42 -0400

    Version final

--- src/interfaces/IBookRepository.ts
+++ src/interfaces/IBookRepository.ts
@@ -1,13 +1,12 @@
-interface Books  {
-    id: number;
-    name: string;
-  }
-  
-
 export default interface IBookRepository {
   books: Array<Books>;
 
   getBook(): Books[];
 
-  getBookById(id : number): Books;
+  getBookById(id: number): Books;
+}
+
+interface Books {
+  id: number;
+  name: string;
 }
